c133f5bf8dcb3e653b410f4af5036fefc44a4bf2
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-04-25 17:49:16 +0100

    Create .nojekyll during deployment

--- components/socialIcons.tsx
+++ components/socialIcons.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from '~/components/link'
 import contactSettings, { SocialLink } from '~/config/settings/contact'
 
 const IconsContainer = styled.span<Props>`
@@ -17,6 +16,9 @@ const IconsContainer = styled.span<Props>`
     cursor: pointer;
     padding: 0px 0.3em;
   }
+  a {
+    color: ${props => props.iconColor ? props.iconColor : 'inherit'};
+  }
 `
 
 interface Props {
@@ -29,10 +31,7 @@ const SocialIcons: React.FC<Props> = ({ exclusions = [], fontSize, iconColor }) 
   <IconsContainer fontSize={fontSize} iconColor={iconColor} >
     {contactSettings.socialMedia.map((icon: SocialLink, index) => {
       if (exclusions.indexOf(icon.name.toLowerCase()) !== -1) return null
-      return (
-        <Link key={index} href={icon.href}>
-          <span><FontAwesomeIcon icon={icon.img} /></span>
-        </Link>)
+      return <a href={icon.href} key={index} ><FontAwesomeIcon icon={icon.img} /></a>
       }) 
     }
   </IconsContainer>
