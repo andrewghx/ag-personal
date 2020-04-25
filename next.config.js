ae6d8001a703694e7d2b443d402c288b102452a7
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-04-25 15:43:13 +0100

    modified asset prefix

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@ const path = require('path')
 const withCSS = require('@zeit/next-css')
 const isProd = process.env.NODE_ENV === 'production'
 
-console.log(process.env)
-
 module.exports = withCSS({
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname)
     return config
   },
-  assetPrefix: isProd ? '/ag-personal' : ''
+  assetPrefix: isProd ? 'https://andrewghx.github.io/ag-personal' : ''
 })
