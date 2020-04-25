83407d575966f709672e05d2a493829348fbc9f3
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-04-25 16:09:43 +0100

    Temporarily removed asset config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const withCSS = require('@zeit/next-css')
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withCSS({
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname)
     return config
   },
-  assetPrefix: isProd ? 'https://andrewghx.github.io/ag-personal' : ''
+  assetPrefix: '' // isProd ? 'https://andrewghx.github.io/ag-personal' : ''
 })
