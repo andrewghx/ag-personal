edfcfee897a1a02fb0fe75192c7ad566bd360e7b
Andrew Griffiths <andrew.griffiths@holidayextras.com>
2020-04-25 15:36:42 +0100

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const withCSS = require('@zeit/next-css')
 const isProd = process.env.NODE_ENV === 'production'
 
+console.log(process.env)
+
 module.exports = withCSS({
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname)
